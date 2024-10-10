f821e9e0d4992a2813b7fd93b25ea3356d191f7a
protarie125 <protarie@gmail.com>
2024-10-10 22:37:31 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <bit>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = int64_t;
+using vl = vector<ll>;
+
+ll n;
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  cin >> n;
+  if (n % 2 == 0) {
+    cout << n / 2;
+  } else {
+    cout << -(n + 1) / 2;
+  }
+
+  return 0;
+}
